ba2ab970cc6a4ef3f18ec7bcee753b8bbfc8f8d7
윤여원 <dudnjsckrgo@gmail.com>
2024-08-06 15:20:53 +0900

    add 초기 설정

---
+++ src/main/java/com/outsider/weekstudy_0810/WeekStudy0810Application.java
@@ -0,0 +1,13 @@
+package com.outsider.weekstudy_0810;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekStudy0810Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekStudy0810Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/outsider/weekstudy_0810/WeekStudy0810ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outsider.weekstudy_0810;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekStudy0810ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
